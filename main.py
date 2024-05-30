19bb6334b51eb98aee8b7dbee59109b083d7faf3
asando1 <alain.sandoz@universite-paris-saclay.fr>
2024-05-30 15:24:45 +0200

    mov

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.player.update()PLAYER_SPEED
+        self.player.update()
 
     def draw(self):
         pyxel.cls(5)
@@ -34,19 +34,37 @@ class Player:
         self.y = y
         self.life = 5
         self.cSprite = s1
+        self.direction = 1
+        self.cursor = 0
 
     def update(self):
         self.move()
         
     def move(self):
+        isMoving = False
+    
         if pyxel.btn(pyxel.KEY_Z):
             self.y -= PLAYER_SPEED
+            isMoving = True
         if pyxel.btn(pyxel.KEY_S):
             self.y += PLAYER_SPEED
+            isMoving = True
         if pyxel.btn(pyxel.KEY_Q):
             self.x -= PLAYER_SPEED
+            isMoving = True
+            self.direction = -1
         if pyxel.btn(pyxel.KEY_D):
             self.x += PLAYER_SPEED
+            isMoving = True
+            self.direction = 1
+
+        if pyxel.frame_count % (FPS/8) == 0 and isMoving:
+            self.cSprite = (0, self.cursor, 136, self.direction*16, 16, 5)
+            
+            if(self.cursor < 48):
+                self.cursor += 16
+            else:
+                self.cursor = 0
         
 
     def draw(self):
