f7ed6bd91064cb110f6400fcd0298f50cb5694d4
amerie1 <ash.merienne@universite-paris-saclay.fr>
2024-05-30 19:07:08 +0200

    holy fucking dhit its bonkers

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pyxel
 # CONSTANTES APP
 HEIGHT = 256
 WIDTH = 256
-FPS = 128
+FPS = 60
+NEST_W = 40
 
 # icones
 fullHeart = (0,48,216,15,15)
@@ -31,7 +32,6 @@ spriteDiver = (0,0,8,16,16)
 # ---------- FONCTIONS GLOBALES ----------
 
 def normalizeVector(x : int, y : int) -> tuple:
-    
     if abs(x) > abs(y):
         if x != 0:
             return(x/abs(x), y/abs(x))
@@ -44,6 +44,24 @@ def normalizeVector(x : int, y : int) -> tuple:
 def abs(x):
     return x*pyxel.sgn(x)
 
+def collision(r1 : tuple, r2 : tuple) -> bool:
+    # (x, y, w, h)
+    x1 = abs(r1[0])
+    y1 = abs(r1[1])
+    w1 = abs(r1[2])
+    h1 = abs(r1[3])
+    x2 = abs(r2[0])
+    y2 = abs(r2[1])
+    w2 = abs(r2[2])
+    h2 = abs(r2[3])
+
+    if (x2 < x1 < (x2+w2)) or (x2 < (x1+w1) < (x2+w2)):
+        if y2 < y1 < (y2+h2):
+            return True
+        elif y2 < (y1+h1) < (y2+h2):
+            return True
+    return False
+
 # ----------------- CLASSES -------------------
 
 class App:
@@ -53,14 +71,76 @@ class App:
         pyxel.mouse(True)
         self.x = 0
         self.player = Player(50, 50)
+        self.flies = []
+        self.nest = Nest()
         pyxel.run(self.update, self.draw)
 
     def update(self):
         self.player.update()
+        #self.nectar.update()
+        #self.nest.update()
+        self.spawnFlies()
+        
+        # updates projectiles
+        # joueur
+        for bullet in self.player.bullets:
+            """for diver in self.divers:
+                if collision(bullet, diver):
+                    self.joueur.removeLife(1)
+                    self.player.bullets.remove(bullet)"""
+            
+            for fly in self.flies:
+                if collision(bullet.rect(), fly.rect()):
+                    if (fly.removeLife(1)):
+                        self.flies.remove(fly)
+                    self.player.bullets.remove(bullet)
+            
+            if bullet != None:
+                bullet.update()
+
+        # update mouches
+        for fly in self.flies:
+            fly.update()
+            if collision(fly.rect(), self.nest.rect()):
+                "qsrbeufrgufzzguzerfguii"
+                self.flies.remove(fly)
+                self.nest.removeLife(2)
+
 
     def draw(self):
         pyxel.cls(5)
         self.player.draw()
+        #self.nectar.draw()
+        #self.nest.draw()
+
+        # draw mouches
+        for fly in self.flies:
+            fly.draw()
+
+    def spawnFlies(self):
+        if (pyxel.frame_count % 55 == 0):
+            self.flies.append(Fly(WIDTH,pyxel.rndi(10,HEIGHT-15)))
+
+# ------------------------------------------------
+
+class Nest:
+    def __init__(self) -> None:
+        self.life = 50
+        self.width = 40
+    
+    def addLife(self, i):
+        self.life += i
+
+    def removeLife(self, dmg) -> bool:
+        if self.life-dmg > 1:
+            self.life -= dmg
+            return False
+        else:
+            return True
+    
+    def rect(self):
+        return (0,0,self.width+2,HEIGHT)
+
 
 # ------------------------------------------------
 
@@ -73,7 +153,7 @@ class Icon:
     def update(self) -> None:
         if (pyxel.frame_count % (FPS/2) == 0):
             self.y += 1
-        if (pyxel.frame_count % FPS == 0):
+        if ((pyxel.frame_count % FPS) == 0):
             self.y -= 2
 
     def draw(self) -> None:
@@ -116,7 +196,7 @@ class Player:
 
         if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT) and pyxel.frame_count - self.timer > COOLDOWN*FPS:
             self.timer = pyxel.frame_count
-            bull = bullet(self.x, self.y, webBall)
+            bull = Bullet(self.x, self.y, webBall)
             self.bullets.append(bull)
             
 
@@ -124,6 +204,12 @@ class Player:
             bull.update()
             if bull.touchBorder():
                 self.bullets.remove(bull)
+
+    def removeLife(self, dmg):
+        if self.life > 0:
+            self.life -= dmg
+        else:
+            self.die()
 
     def draw(self):
         for bullet in self.bullets:
@@ -181,6 +267,9 @@ class Player:
             self.y = 0
         elif self.y > HEIGHT - 16:
             self.y = HEIGHT - 16
+    
+    def rect(self) -> tuple:
+        return (self.x, self.y, 16, 16)
 
     def anim(self, first_x : int, first_y : int, nbFrame : int, speed : int):
         if pyxel.frame_count % (FPS/speed) == 0:
@@ -211,6 +300,9 @@ class PowerUp:
             self.cooldown_current -= 1
             if self.cooldown_current == 0:
                 self.visible = True
+    
+    def rect(self) -> tuple:
+        return (self.x, self.y, 12, 12)
 
     def draw(self) -> None:
         if self.visible:
@@ -229,13 +321,12 @@ BULLET_SPEED = 100/FPS
 #SPRITES
 webBall = (0,131,193,4,4,5)
 
-class bullet:
+class Bullet:
     def __init__(self, x : float, y : float, sprite) -> None:
         self.x = x + 4
         self.y = y + 4
         self.sprite = sprite
         self.direction = self.mouseDirection()
-        print(self.mouseDirection())
 
     def update(self):
         self.x += BULLET_SPEED*self.direction[0]
@@ -258,6 +349,9 @@ class bullet:
         elif self.y > HEIGHT:
             return True
         return False
+    
+    def rect(self) -> tuple:
+        return (self.x, self.y, 4, 4)
         
     def draw(self) -> None:
         pyxel.blt(self.x, self.y, self.sprite[0], webBall[1], self.sprite[2], self.sprite[3], self.sprite[4], self.sprite[5])
@@ -268,47 +362,52 @@ class Fly :
     def __init__(self, x:int,y:int):
         self.x = x
         self.y = y
-        self.live = 2
+        self.life = 2
         self.cursor = 0
-        self.cSprite = fly
-        self.flyListe= []
+        self.cSprite = fly # constante glob
         
     def draw (self):
-        pyxel.blt(self.x, self.y, self.cSprite[0], self.cSprite[1], self.cSprite[2], self.cSprite[3], self.cSprite[4], self.cSprite[5])
+        pyxel.blt(self.x, self.y, self.cSprite[0], self.cSprite[1], self.cSprite[2], self.cSprite[3], self.cSprite[4], 5)
 
     def touchBorder(self):
         if self.x < 0:
             self.x = 0
         elif self.x > WIDTH - 16:
             self.x = WIDTH - 16
-
         if self.y < 0:
             self.y = 0
         elif self.y > HEIGHT - 16:
             self.y = HEIGHT - 16
     
-    def flyCreation (self):
-        if (FPS % 30 == 0):
-            self.flyListe.append([0,pyxel.rndi(10,145)])
+    def removeLife(self, dmg) -> bool:
+        if self.life > 1:
+            self.life -= dmg
+            return False
+        else:
+            return True
 
-
-
-    def flyMovement(self):
-        for fly in self.flyListe :
-            if (fly[0]<62):
-                fly[0]+=1
-                self.moveAnim(128,8,8,)
-            else :
-                self.flyListe.remove(fly)
+    def update(self):
+        self.move()
+    
+    def move(self):
+        if (pyxel.frame_count % 5 == 0):
+            if (self.x > NEST_W):
+                self.x -= 1
+                self.anim(128,8,8,2)
 
     def anim(self, first_x : int, first_y : int, nbFrame : int, speed : int):
-        if pyxel.frame_count % (FPS/speed) == 0:
-            self.cSprite = (0, first_x + self.cursor, first_y, self.direction*16, 16, 5)
+        if (pyxel.frame_count % 30) == 0:
+            self.cSprite = (0, first_x + self.cursor, first_y, 16, 16, 5)
             
             if(self.cursor < (nbFrame-1)*16):
                 self.cursor += 16
             else:
                 self.cursor = 0
+        # REMOVE
+        self.cSprite = fly
+    
+    def rect(self) -> tuple:
+        return (self.x, self.y, 16, 16)
 
 
 
